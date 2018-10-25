77d4350295c09ebb3dafa7cbb162566dcef23bc7
Lucian Gutoiu <lucian.gutoiu@pitechnologies.ro>
2018-10-25 14:27:28 +0300

    wheel working V1

--- ajax/options.php
+++ ajax/options.php
@@ -5,8 +5,17 @@ header("Content-Type: application/json; charset=UTF-8");
 $options = [
 	'op1' => 'option1',
 	'op2' => 'option2',
+	'op3' => 'option3',
+	'op4' => 'option4',
+	'op5' => 'option5',
+	'op6' => 'option6',
+	'op7' => 'option7',
+	'op8' => 'option8',
+	'op9' => 'option9',
+	'op10' => 'option10',
 ];
 
 http_response_code(200);
 echo json_encode($options);
+exit;
 ?>
